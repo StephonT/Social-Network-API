f533c41e4cd5b8ce9f16c1ea4a3f3fb73ee0f66c
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-09-05 15:03:55 -0400

    changed addFriend to .put

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -27,7 +27,7 @@ router
 // /api/users/:userId/friends/:friendId
 router
       .route('/:id/friends/:friendId')
-      .post(addFriend)
+      .put(addFriend)
       .delete(deleteFriend)
 
 module.exports = router;   
